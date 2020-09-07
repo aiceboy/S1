841bb4a8eb062c68fa0d3dc8aa900ebb56cd2b25
TheRobertSnow <robertsnaer901@gmail.com>
2020-09-07 19:04:45 +0000

    first commit

---
+++ src/main/java/Percolation.java
@@ -0,0 +1,23 @@
+public class Percolation {
+    public Percolation(int N) { // create N-by-N grid, with all sites initially blocked
+
+    }
+    public void open(int row, int col) { // open the site (row, col) if it is not open already
+
+    }
+    public boolean isOpen(int row, int col) { // is the site (row, col) open?
+        return false;
+    }
+    public boolean isFull(int row, int col) { // is the site (row, col) full?
+        return false;
+    }
+    public int numberOfOpenSites() { // number of open sites
+        return 0;
+    }
+    public boolean percolates() { // does the system percolate?
+        return false;
+    }
+    public static void main(String[] args) { // unit testing (required)
+        System.out.println("Hello, world!");
+    }
+}
